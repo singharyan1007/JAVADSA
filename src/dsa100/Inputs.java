51426dbe412a0e28b1410f6be9575dc341a2893d
Aryan <singh.aryan837@gmail.com>
2022-10-17 09:26:44 -0400

    Dsa

---
+++ src/dsa100/Inputs.java
@@ -0,0 +1,13 @@
+package dsa100;
+
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("Please some input : ");
+        int rolling=input.nextInt();
+        System.out.println("Roll "+ rolling);
+
+    }
+}
