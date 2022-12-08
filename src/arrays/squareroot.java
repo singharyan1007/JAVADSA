b78f5f863e20cc79c836af953a08e0f417319c75
Aryan Singh <singh.aryan837@gmail.com>
2022-12-08 21:52:04 +0530

    java dsa

---
+++ src/arrays/squareroot.java
@@ -0,0 +1,10 @@
+package arrays;
+
+public class squareroot {
+    public static void main(String[] args) {
+        int x=3;
+        int ans= x^(1/2);
+        System.out.println(ans);
+
+    }
+}
